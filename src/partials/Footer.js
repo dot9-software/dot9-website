8e8f769112f00c18af9f126ca7df2121a157cdbe
zirkler <zirklerchristof@gmail.com>
2023-02-04 09:09:57 +0100

    wip

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -5,92 +5,106 @@ import { useTranslation } from "react-i18next";
 import Logo from "./Logo";
 
 function Footer() {
+    const { t } = useTranslation();
 
-  const { t } = useTranslation();
+    return (
+        <footer>
+            <div className="max-w-6xl mx-auto px-4 sm:px-6">
+                {/* Top area: Blocks */}
+                <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
+                    {/* Logo Block */}
+                    <div className="sm:col-span-12 lg:col-span-3">
+                        <div className="mb-2">
+                            <Logo width="80" height="40" />
+                        </div>
+                        <div className="text-sm text-gray-600">
+                            <Link
+                                to="/terms"
+                                className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out"
+                            >
+                                {t("footer-imprint-link")}
+                            </Link>{" "}
+                            ·{" "}
+                            <Link
+                                to="/privacy"
+                                className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out"
+                            >
+                                {t("footer-privacy-policy-link")}
+                            </Link>
+                        </div>
+                    </div>
 
-  return (
-    <footer>
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+                    {/* Company Block */}
+                    <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
+                        <h6 className="text-gray-800 font-medium mb-2">
+                            {t("footer-company-block-title")}
+                        </h6>
+                        <ul className="text-sm">
+                            <li className="mb-2">
+                                <Link
+                                    to="/"
+                                    className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                                >
+                                    {t("nav-home")}
+                                </Link>
+                            </li>
+                            <li className="mb-2">
+                                <Link
+                                    to="/about"
+                                    className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                                >
+                                    {t("nav-about")}
+                                </Link>
+                            </li>
+                            <li className="mb-2">
+                                <Link
+                                    to="/careers"
+                                    className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                                >
+                                    {t("nav-careers")}
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
 
-        {/* Top area: Blocks */}
-        <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
-          
-          {/* Logo Block */}
-          <div className="sm:col-span-12 lg:col-span-3">
-            <div className="mb-2">
-              <Logo width="80" height="40" />
+                    {/* Schedule Video Call Button */}
+                    <div className="sm:col-span-12 md:col-span-12 lg:col-span-7 lg:justify-self-end">
+                        <a
+                            className="btn text-white bg-d9Blue-900 hover:bg-d9Blue-800 shadow"
+                            href="https://calendly.com/dot9"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <svg
+                                className="h-6 w-6 mr-2"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                                ></path>
+                            </svg>
+                            {t("schedule-call-button")}
+                        </a>
+                    </div>
+                </div>
+
+                {/* Bottom area */}
+                <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
+                    {/* Copyrights note */}
+                    <div className="text-sm text-gray-600 mr-4">
+                        <p>{t("footer-copyright-notice")}</p>
+                    </div>
+                </div>
             </div>
-            <div className="text-sm text-gray-600">
-              <Link
-                to="/terms"
-                className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out"
-              >
-                {t('footer-imprint-link')}
-              </Link>{" "}
-              ·{" "}
-              <Link
-                to="/privacy"
-                className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out"
-              >
-                {t('footer-privacy-policy-link')}
-              </Link>
-            </div>
-          </div>
-
-          {/* Company Block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">{t('footer-company-block-title')}</h6>
-            <ul className="text-sm">
-              <li className="mb-2">
-                <Link to="/" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">{t('nav-home')}</Link>
-              </li>
-              <li className="mb-2">
-                <Link to="/about" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">{t('nav-about')}</Link>
-              </li>
-              <li className="mb-2">
-                <Link to="/careers" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">{t('nav-careers')}</Link>
-              </li>
-            </ul>
-          </div>
-
-          {/* Schedule Video Call Button */}
-          <div className="sm:col-span-12 md:col-span-12 lg:col-span-7 lg:justify-self-end">
-          <a
-            className="btn text-white bg-d9Blue-900 hover:bg-d9Blue-800 shadow"
-            href="https://dot9.appointlet.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <svg
-              className="h-6 w-6 mr-2"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-              ></path>
-            </svg>
-            {t('schedule-video-call-button')}
-          </a>
-        </div>
-        </div>
-
-        {/* Bottom area */}
-        <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
-          {/* Copyrights note */}
-          <div className="text-sm text-gray-600 mr-4">
-            <p>{t('footer-copyright-notice')}</p>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+        </footer>
+    );
 }
 
 export default Footer;
--- src/partials/Testimonials.js
+++ src/partials/Testimonials.js
@@ -3,19 +3,19 @@ import Testimonial from "../components/Testimonial";
 import { useTranslation } from "react-i18next";
 
 function Testimonials() {
-
     const { t } = useTranslation();
-    const [testimonials, setTestimonials] = useState([])
 
-    useEffect(() => {
-        async function fetchTestimonials() {
-            const url = "https://dot9-website-strapi.herokuapp.com/testimonials?_sort=sortPriority:ASC"
-            const testimonialsResponse = await fetch(url)
-            const data = await testimonialsResponse.json() 
-            setTestimonials(data)
-        }
-        fetchTestimonials()
-    }, [])
+    const offlineTestimonials = [
+        {
+            id: "1",
+            text: "Working with dot9 has been great for our business. We have been able to land some significant contracts with dot9's help, and we’ll definitely expand our cooperation with them. We have been really impressed by the quality of services we received. Blue Canvas can definitely recommend dot9 to everyone looking for high-quality, highly-flexible engineering solutions. They worked on integrating Blue Canvas with external services, and what was particularly delightful was the quality and independence with which they designed and implemented those integrations across dozens of technologies (Java, React, Express.js etc...) and hard-to-understand APIs (Jira, Azure). They charge reasonable prices, are professional and courteous in dealings, and deliver our items, even ones outside the original scope.",
+            authorName: "Jack Moxon",
+            authorCompany: "Blue Canvas",
+            authorCompanyUrl: "https://bluecanvas.io",
+            authorRole: "CEO and Co-Founder",
+            authorImageUrl: require("../images/jack_moxon.jpg").default,
+        },
+    ];
 
     return (
         <section className="relative">
@@ -23,9 +23,7 @@ function Testimonials() {
                 <div className="py-12 md:py-20">
                     {/* Section header */}
                     <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-                        <h2 className="h2">
-                            {t('testimonials-title')}
-                        </h2>
+                        <h2 className="h2">{t("testimonials-title")}</h2>
                     </div>
 
                     {/* Testimonials */}
@@ -33,14 +31,16 @@ function Testimonials() {
                         className="max-w-3xl mx-auto mt-10"
                         data-aos="zoom-y-out"
                     >
-                        {testimonials.map((testimonial, index) => {
+                        {offlineTestimonials.map((testimonial, index) => {
                             return (
                                 <Testimonial
                                     key={testimonial.id}
-                                    text={testimonial.textPreview}
-                                    textMore={testimonial.textExtended}
+                                    text={testimonial.text}
+                                    textMore={testimonial.text}
                                     authorName={testimonial.authorName}
-                                    authorImageUrl={testimonial.authorImage?.url || ""}
+                                    authorImageUrl={
+                                        testimonial.authorImage?.url || ""
+                                    }
                                     authorDescription={testimonial.authorRole}
                                     companyName={testimonial.authorCompany}
                                     companyUrl={testimonial.authorCompanyUrl}
